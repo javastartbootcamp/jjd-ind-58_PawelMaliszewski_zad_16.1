34be54897f4d61413437c0efb6b2029ff849575e
Pawel Maliszewski <pablo999455@gmail.com>
2023-05-30 14:23:34 +0200

    solution

--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -1,12 +1,29 @@
 package pl.javastart.task;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class SeasonManager {
 
     // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
-
+        printSeasons();
+        askForTheSeasonOfTheYearAndPrintItsMonths(scanner);
     }
 
+    private static void askForTheSeasonOfTheYearAndPrintItsMonths(Scanner scanner) {
+        try {
+            Season season = Season.seasonFromString(scanner.nextLine());
+            Objects.requireNonNull(season).printMonths();
+        } catch (NullPointerException e) {
+            System.err.println("Niewłaściwy wybór");
+        }
+    }
+
+    private static void printSeasons() {
+        System.out.println("Podaj porę roku:");
+        for (Season value : Season.values()) {
+            System.out.println(value.getDescription());
+        }
+    }
 }
